4e0e158e5769b7ae1b5237498838d16f4a9eae42
Miguel Vargas <mfvg1920@gmail.com>
2020-07-14 23:57:09 -0400

    adding other user

--- myblog/views.py
+++ myblog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Welcome to MyBlog! miguelvargasg Assignement 1...')
+    return HttpResponse('Welcome to MyBlog! miguelvargasg Assignement 1...adding tester1/tester1')
