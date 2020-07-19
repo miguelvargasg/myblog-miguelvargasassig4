dfb85d277f4a140848cc1d7a6b91080e9a0d330f
Miguel Vargas <mfvg1920@gmail.com>
2020-07-19 12:21:05 -0400

    updating author foreign key

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,12 @@ class Post(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='blog_posts',
+        null=True
+    )
 
     def __str__(self):
         return self.title
